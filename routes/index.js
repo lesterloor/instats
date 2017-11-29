0cabb382be59e58084c57957cb87092d37091cbe
lester <lester@Charless-iMac.home>
2017-11-29 04:58:27 -0500

    Added recent 5 post data of searched user

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,17 @@ router.post('/users', function(req, res, next) {
   console.log(searchUser);
   axios.get('https://api.instagram.com/v1/users/search?q='+searchUser+'&access_token='+access_token)
   .then(function (response) {
+    console.log(response.data.data[0].id);
+    let insta_userId = response.data.data[0].id;
+      axios.get('https://api.instagram.com/v1/users/'+insta_userId+'/media/recent/?access_token='+access_token+'&count=5')
+      .then(function (mediaResponse) {
 
-    console.log(response.data.data[0]);
-    res.render('users', { data: response.data.data[0] });
+        console.log(mediaResponse.data.data);
+        res.render('users', { data: response.data.data[0],media: mediaResponse.data.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   })
   .catch(function (error) {
     console.log(error);
